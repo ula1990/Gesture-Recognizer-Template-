82a23ae57429899376ea95d23a269ac9fc1b12da
Uladzislau Daratsiuk <uladzislaudaratsiuk@uladzislaus-mbp.root.tradedoubler.com>
2018-02-13 13:49:22 +0100

    Adding files

--- GR Tutorial/ViewController.swift
+++ GR Tutorial/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var fileImage: UIImageView!
+    @IBOutlet weak var trashImage: UIImageView!
+    
+    var fileViewOrgin: CGPoint!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addPanGesture(view: fileImage)
+        fileViewOrgin = fileImage.frame.origin
+        view.bringSubview(toFront: fileImage)
+     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    
+    func addPanGesture(view: UIView){
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+        
+        
     }
-
+    
+    //Refactor
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer){
+        
+        let fileView = sender.view!
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            
+            fileView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+            
+        case .ended:
+            if fileView.frame.intersects(trashImage.frame){
+                deleteImage(view: fileImage)
+            }else {
+                returnOriginPos(view: fileImage)
+            }
+        default:
+            break
+        }
+        
+        
+    }
+    
+    func deleteImage(view: UIView) {
+        UIView.animate(withDuration: 0.3, animations: {
+            view.alpha = 0.0
+        })
+    }
+    
+    
+    func returnOriginPos(view:UIView){
+        UIView.animate(withDuration: 0.3, animations: {
+            view.frame.origin = self.fileViewOrgin
+        })
+        
+    }
 
 }
 
